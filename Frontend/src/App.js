b14c337c95bda2f036b6dd6185b4d0cfb476109d
Eric Smalley <eric@smalley.nl>
2023-02-20 06:36:49 +1300

    Move the Add functionality to its own AddForm component. Pass the API in, as well as the todoItems state, so that when adding the local state can instantly be updated as well. Added validation for a minimum amount of characters (5) for the TODO description. Handled potential errors coming back from the server. Check for a response with data in it, otherwise show the general error message. Posted a new TODO item to the API, and then modified local todoItems state.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,53 +1,22 @@
 import './App.css';
-import { Image, Button, Container, Row, Col, Form, Table, Stack } from 'react-bootstrap';
+import { Image, Button, Container, Row, Col, Table } from 'react-bootstrap';
 import React, { useState, useEffect } from 'react';
+import AddForm from './components/AddForm';
 import Instructions from './components/Instructions';
 import Footer from './components/Footer';
 
 const App = ({ todoListApi }) => {
-  const [description, setDescription] = useState('');
-  const [items, setItems] = useState([]);
+  const [todoItems, setTodoItems] = useState([]);
 
   useEffect(() => {
     // todo
   }, []);
 
-  const renderAddTodoItemContent = () => {
-    return (
-      <Container>
-        <h1>Add Item</h1>
-        <Form.Group as={Row} className="mb-3" controlId="formAddTodoItem">
-          <Form.Label column sm="2">
-            Description
-          </Form.Label>
-          <Col md="6">
-            <Form.Control
-              type="text"
-              placeholder="Enter description..."
-              value={description}
-              onChange={handleDescriptionChange}
-            />
-          </Col>
-        </Form.Group>
-        <Form.Group as={Row} className="mb-3 offset-md-2" controlId="formAddTodoItem">
-          <Stack direction="horizontal" gap={2}>
-            <Button variant="primary" onClick={() => handleAdd()}>
-              Add Item
-            </Button>
-            <Button variant="secondary" onClick={() => handleClear()}>
-              Clear
-            </Button>
-          </Stack>
-        </Form.Group>
-      </Container>
-    );
-  };
-
   const renderTodoItemsContent = () => {
     return (
       <>
         <h1>
-          Showing {items.length} Item(s){' '}
+          Showing {todoItems.length} Item(s){' '}
           <Button variant="primary" className="pull-right" onClick={() => getItems()}>
             Refresh
           </Button>
@@ -62,7 +31,7 @@ const App = ({ todoListApi }) => {
             </tr>
           </thead>
           <tbody>
-            {items.map((item) => (
+            {todoItems.map((item) => (
               <tr key={item.id}>
                 <td>{item.id}</td>
                 <td>{item.description}</td>
@@ -79,28 +48,12 @@ const App = ({ todoListApi }) => {
     );
   };
 
-  const handleDescriptionChange = (event) => {
-    // todo
-  };
-
   async function getItems() {
     try {
       alert('todo');
     } catch (error) {
       console.error(error);
     }
-  }
-
-  async function handleAdd() {
-    try {
-      alert('todo');
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  function handleClear() {
-    setDescription('');
   }
 
   async function handleMarkAsComplete(item) {
@@ -125,7 +78,9 @@ const App = ({ todoListApi }) => {
           </Col>
         </Row>
         <Row>
-          <Col>{renderAddTodoItemContent()}</Col>
+          <Col>
+            <AddForm todoListApi={todoListApi} todoItems={todoItems} setTodoItems={setTodoItems} />
+          </Col>
         </Row>
         <br />
         <Row>
---
+++ Frontend/src/components/AddForm.js
@@ -0,0 +1,89 @@
+import { Alert, Button, Col, Container, Form, Row, Stack } from 'react-bootstrap';
+import React, { useState } from 'react';
+
+export default function AddForm({ todoListApi, todoItems, setTodoItems }) {
+  const [description, setDescription] = useState('');
+  const [addDescriptionError, setAddDescriptionError] = useState('');
+
+  async function handleAdd() {
+    try {
+      if (description.length < 5) {
+        setAddDescriptionError('Please describe your TODO with a minimum of 5 characters');
+        return;
+      }
+
+      setAddDescriptionError('');
+
+      // Create via the API
+      const response = await todoListApi.create({
+        description: description,
+        complete: false,
+      });
+
+      setDescription('');
+
+      // Also add to the local representation
+      try {
+        appendTodoItem(response);
+      } catch (error) {
+        setAddDescriptionError(
+          `The todo item was successfully added, but the local table could not be properly updated. Please hit refresh.`
+        );
+      }
+    } catch (error) {
+      setAddDescriptionError(
+        `Could not add the todo item due to a server error: ${
+          error.response?.data ? error.response.data : error.message
+        }`
+      );
+    }
+  }
+
+  const appendTodoItem = (todoItem) => {
+    const updatedTodoItems = [...todoItems, todoItem];
+
+    setTodoItems(updatedTodoItems);
+  };
+
+  function handleClear() {
+    setDescription('');
+  }
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
+
+  return (
+    <Container>
+      <h1>Add Item</h1>
+      <Form.Group as={Row} className="mb-3" controlId="formAddTodoItem">
+        <Form.Label column sm="2">
+          Description
+        </Form.Label>
+        <Col md="6">
+          <Form.Control
+            type="text"
+            placeholder="Describe your TODO with a minimum of 5 characters"
+            value={description}
+            onChange={handleDescriptionChange}
+          />
+          {addDescriptionError && (
+            <Alert className="mt-3" variant="danger">
+              {addDescriptionError}
+            </Alert>
+          )}
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mb-3 offset-md-2" controlId="formAddTodoItem">
+        <Stack direction="horizontal" gap={2}>
+          <Button variant="primary" onClick={() => handleAdd()}>
+            Add Item
+          </Button>
+          <Button variant="secondary" onClick={() => handleClear()}>
+            Clear
+          </Button>
+        </Stack>
+      </Form.Group>
+    </Container>
+  );
+}
