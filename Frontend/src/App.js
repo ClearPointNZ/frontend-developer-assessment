df47dd982a1d03eff5cc4e9e7e71d82b89ff83f6
Eric Smalley <eric@smalley.nl>
2023-02-20 06:05:20 +1300

    Move the instructions and the footer to their own components

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
-import { Image, Alert, Button, Container, Row, Col, Form, Table, Stack } from 'react-bootstrap';
+import { Image, Button, Container, Row, Col, Form, Table, Stack } from 'react-bootstrap';
 import React, { useState, useEffect } from 'react';
+import Instructions from './components/Instructions';
+import Footer from './components/Footer';
 
 const axios = require('axios');
 
@@ -121,25 +123,7 @@ const App = () => {
         </Row>
         <Row>
           <Col>
-            <Alert variant="success">
-              <Alert.Heading>Todo List App</Alert.Heading>
-              Welcome to the ClearPoint frontend technical test. We like to keep things simple, yet clean so your
-              task(s) are as follows:
-              <br />
-              <br />
-              <ol className="list-left">
-                <li>Add the ability to add (POST) a Todo Item by calling the backend API</li>
-                <li>
-                  Display (GET) all the current Todo Items in the below grid and display them in any order you wish
-                </li>
-                <li>
-                  Bonus points for completing the 'Mark as completed' button code for allowing users to update and mark
-                  a specific Todo Item as completed and for displaying any relevant validation errors/ messages from the
-                  API in the UI
-                </li>
-                <li>Feel free to add unit tests and refactor the component(s) as best you see fit</li>
-              </ol>
-            </Alert>
+            <Instructions />
           </Col>
         </Row>
         <Row>
@@ -150,14 +134,7 @@ const App = () => {
           <Col>{renderTodoItemsContent()}</Col>
         </Row>
       </Container>
-      <footer className="page-footer font-small teal pt-4">
-        <div className="footer-copyright text-center py-3">
-          © 2021 Copyright:
-          <a href="https://clearpoint.digital" target="_blank" rel="noreferrer">
-            clearpoint.digital
-          </a>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 };
---
+++ Frontend/src/components/Instructions.js
@@ -0,0 +1,24 @@
+import { Alert } from 'react-bootstrap';
+import React from 'react';
+
+export default function Instructions() {
+  return (
+    <Alert variant="success">
+      <Alert.Heading>Todo List App</Alert.Heading>
+      Welcome to the ClearPoint frontend technical test. We like to keep things simple, yet clean so your task(s) are as
+      follows:
+      <br />
+      <br />
+      <ol className="list-left">
+        <li>Add the ability to add (POST) a Todo Item by calling the backend API</li>
+        <li>Display (GET) all the current Todo Items in the below grid and display them in any order you wish</li>
+        <li>
+          Bonus points for completing the 'Mark as completed' button code for allowing users to update and mark a
+          specific Todo Item as completed and for displaying any relevant validation errors/ messages from the API in
+          the UI
+        </li>
+        <li>Feel free to add unit tests and refactor the component(s) as best you see fit</li>
+      </ol>
+    </Alert>
+  );
+}
